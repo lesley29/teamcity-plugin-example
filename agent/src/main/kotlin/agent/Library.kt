e4b604e2f3951f9292a1289ac93ecdc0d941f4a2
Vladislav Grinin <vladislav.grinin@jetbrains.com>
2022-12-09 12:57:06 +0400

    init

---
+++ agent/src/main/kotlin/agent/Library.kt
@@ -0,0 +1,7 @@
+package agent
+
+class Library {
+    fun someLibraryMethod(): Boolean {
+        return true
+    }
+}
---
+++ server/src/main/kotlin/server/Library.kt
@@ -0,0 +1,7 @@
+package server
+
+class Library {
+    fun someLibraryMethod(): Boolean {
+        return true
+    }
+}
